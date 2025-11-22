8db286ba82786dbd4a865c5abc5a504a063a26a0
Amitoj Dhillon <Dhiay010@mymail.unisa.edu.au>
2025-11-22 14:56:20 +1030

    Changes to staff.py

--- staff.py
+++ staff.py
@@ -1,8 +1,40 @@
 '''
 File: filename.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
+Author: Amitoj Dhillon
 ID: 110100110
-Username: bizvy001
+Username: Dhiay010
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+from animal import Animal
+from enclosure import Enclosure
+
+class Staff:
+
+    def __init__(self, name, staff_id, role):
+        self.__name = name
+        self.__staff_id = staff_id
+        self.__role = role
+
+
+    def get_name(self):
+        return self.__name
+
+    def get_staff_id(self):
+        return self.__staff_id
+
+    def get_role(self):
+        return self.__role
+
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_staff_id(self, staff_id):
+        self.__staff_id = staff_id
+
+    def set_role(self, role):
+        self.__role = role
+
+    def __str__(self):
+        return f"{self.__name} {self.__role}"
