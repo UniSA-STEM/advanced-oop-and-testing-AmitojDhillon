e03f6d94f3a7d557c7a16b8673bd79fab6c7bde9
Amitoj Dhillon <Dhiay010@mymail.unisa.edu.au>
2025-11-23 15:05:45 +1030

    Changes to staff.py

--- staff.py
+++ staff.py
@@ -37,4 +37,37 @@ class Staff:
         self.__role = role
 
     def __str__(self):
-        return f"{self.__name} {self.__role}
+        return f"{self.__name} {self.__role}"
+
+class Zookeeper(Staff):
+    def __init__(self, name, staff_id):
+        super().__init__(name, staff_id, "Zookeeper")
+        self.__enclosures = []
+
+    def get_enclosure(self):
+        return list(self.__enclosures)
+
+    def set_enclosure(self, enclosure):
+        self.__enclosures = enclosures
+
+    def assign_enclosure(self, enclosure):
+        if isinstance(enclosure, Enclosure):
+            if enclosure not in self.__enclosures:
+                self.__enclosures.append(enclosure)
+
+    def feed_animal(self, animal):
+        if isinstance(animal, Animal):
+            return f"{self.__get_name()} feed {animal.__get_name()}. {animal.eat()}"
+
+    def clean_enclosure(self, enclosure):
+        if enclosure in self.__enclosures:
+            enclosure.clean()
+            return f"{self.__get_name()} cleared {enclosure.get_name()}"
+
+    def get_summary(self):
+        if self.__enclosures:
+            names = ", ".join(a.get_name() for a in self.__enclosures)
+        else:
+            names = "None"
+
+        return f"{self.__get_name()} | Enclosures: {names}"
